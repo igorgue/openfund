ca9d631ce5cd33573b9a1f20509762483e915d0a
Chet Bortz <me@chetbortz.com>
2012-10-13 15:59:16 -0400

    Add template for database file and extract registration form

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,2 +1,25 @@
 class CampaignsController < ApplicationController
+
+  before_filter :authenticate_user!, :only => [:new, :update, :delete, :edit]
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+private
+
+  def verify_user
+  end
+
 end
