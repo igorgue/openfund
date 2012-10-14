d6a571dde65e60657afea5fcff1cb9b17f2cefdc
Auston Bunsen <auston.bunsen@gmail.com>
2012-10-14 02:02:28 -0400

    added video migration

---
+++ db/migrate/20121014060158_add_campaign_video_attribute.rb
@@ -0,0 +1,7 @@
+class AddCampaignVideoAttribute < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121013192701) do
+ActiveRecord::Schema.define(:version => 20121014060158) do
 
   create_table "campaign_faqs", :force => true do |t|
     t.integer  "campaign_id"
