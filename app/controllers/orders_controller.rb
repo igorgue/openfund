76ed284640a9d5f794b004e888536f7a81b79601
Alain Fontaine <criticerz@gmail.com>
2012-10-14 16:05:45 -0400

    edited orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,8 +3,18 @@ class OrdersController < ApplicationController
   
   def index
   
-    @orders = Order.all
+    orders = Order.all
+    @orders = orders
+    
+    #orders.each do |o|
+    #  total += o.price
+    #end
+    
+    #puts total
+    
     @campaign = Campaign.find(params[:campaign_id])
+    
+    
     
   end
   
